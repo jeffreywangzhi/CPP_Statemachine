390dd380b03de9ba0c185ad2a7d4b58044345e18
Jeffrey Wang <109072805+jeffreywanggg@users.noreply.github.com>
2023-10-05 13:35:24 +0800

    Create statemachine.cpp

---
+++ statemachine.cpp
@@ -0,0 +1,5 @@
+#include <boost/statechart/event.hpp>
+#include <boost/statechart/state_machine.hpp>
+#include <boost/statechart/simple_state.hpp>
+#include <boost/statechart/transition.hpp>
+#include <boost/statechart/custom_reaction.hpp>
