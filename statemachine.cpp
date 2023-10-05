2c0a757bc4cc47e1b206533e0749ec089a27543a
JeffreyWangz <jeffrey02120212@gmail.com>
2023-10-05 16:39:48 +0800

    add c++ statemachine src

--- statemachine.cpp
+++ statemachine.cpp
@@ -1,5 +1,30 @@
+#include <iostream>
 #include <boost/statechart/event.hpp>
 #include <boost/statechart/state_machine.hpp>
 #include <boost/statechart/simple_state.hpp>
 #include <boost/statechart/transition.hpp>
 #include <boost/statechart/custom_reaction.hpp>
+namespace sc = boost::statechart;
+using namespace std;
+
+struct firstState;
+struct secondState;
+struct statemachine : sc::state_machine<statemachine, secondState>
+{
+	statemachine() { cout << "Starting => statemachine" << endl; }
+};
+
+struct firstState : sc::simple_state<firstState, statemachine>
+{
+	firstState() { cout << "In State => firstState" << endl; }
+};
+struct secondState : sc::simple_state<secondState, statemachine>
+{
+	secondState() { cout << "In State => secondState" << endl; }
+};
+
+int main() {
+	statemachine sm;
+	sm.initiate();
+	return 0;
+}
